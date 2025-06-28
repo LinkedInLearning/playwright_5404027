df6aba39ae23d02fb1790c75388b6e85086bb390
Labasse <labasse@gmail.com>
2025-06-28 16:31:04 +0200

    Remplir un formulaire (début)

--- tests/portrait.spec.ts
+++ tests/portrait.spec.ts
@@ -6,3 +6,23 @@ test('Select "Nez" filters the palette', async ({ page }) => {
   await page.getByRole('combobox').selectOption('Nez');
   await expect(page.locator('#palette').getByRole('listitem')).toHaveCount(6);
 });
+
+test('Set the portrait title', async ({ page }) => {
+  await page.goto('https://labasse.github.io/tutti-frutti/portrait.html');
+  // TODO
+});
+
+test('Set invalid portrait title', async ({ page }) => {
+  await page.goto('https://labasse.github.io/tutti-frutti/portrait.html');
+  // TODO
+});
+
+test('Hide portrait title', async ({ page }) => {
+  await page.goto('https://labasse.github.io/tutti-frutti/portrait.html');
+  // TODO
+});
+
+test('Change default item size', async ({ page }) => {
+  await page.goto('https://labasse.github.io/tutti-frutti/portrait.html');
+  // TODO
+});
