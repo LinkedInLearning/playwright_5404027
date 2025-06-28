468b2a0490bfd34a22da7ead30957f0c58874a23
Labasse <labasse@gmail.com>
2025-06-28 10:39:04 +0200

    Localiser un composant (fin)

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -3,14 +3,14 @@ import { test, expect } from '@playwright/test';
 
 test('All fruits displayed', async ({ page }) => {
   await page.goto('https://labasse.github.io/tutti-frutti/index.html');
-  await expect(page.locator('#liste img')).toHaveCount(11);
+  await expect(page.getByAltText(/\(fruit\)$/)).toHaveCount(11);
 });
 
 test('Simple search with results', async ({ page }) => {
   await page.goto('https://labasse.github.io/tutti-frutti/');
   await page.getByRole('searchbox', { name: 'Chercher' }).click();
   await page.getByRole('searchbox', { name: 'Chercher' }).fill('ana');
-  await expect(page.locator('#liste')).toContainText('AnanasNullam cursus sapien lacus, ut volutpat nunc semper non.BananeAenean non nunc porta, venenatis lacus non, auctor elit.CuisinerNos meilleures recettes :Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur mollis velit sagittis elit malesuada suscipit.In cursus tortor vel erat malesuada, eget tincidunt felis sagittis.Ut iaculis justo eget euismod posuere.Ut consequat neque eget orci malesuada, eget ornare urna vulputate.Nam molestie eros nec iaculis consectetur.');
+  await expect(page.getByText(/^Ananas.*Banane/i)).toBeVisible();
 });
 
 test('Simple search with no result', async ({ page }) => {
