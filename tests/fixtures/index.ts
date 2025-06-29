5c44e22906c7687180832fc93c545faa6a6d1c75
Labasse <labasse@gmail.com>
2025-06-29 23:56:40 +0200

    Préparer une authentification (début)

--- tests/fixtures/index.ts
+++ tests/fixtures/index.ts
@@ -1,8 +1,12 @@
 import { test as base } from '@playwright/test';
+import { User } from './user';
 import { Nav } from './nav';
 
 export const test = base.extend<{
   nav: Nav,
+}, {
+  adminUser: User,
+  normalUser: User,
 }>({
     nav: async ({ page }, use) => { 
         console.log('Fixture nav : Init');
@@ -12,5 +16,25 @@ export const test = base.extend<{
         await use(nav);
 
         console.log('Fixture nav : Shut');
-    }
+    },
+    adminUser: [
+      async ({ browser }, use) => {
+        const user = await User.login(
+          browser, test.info(), 
+          'admin', '(mot de passe)'
+        );
+        await use(user);
+        await user.resetState();
+      }, { scope: 'worker' }
+    ],
+    normalUser: [
+      async ({ browser }, use) => {        
+        const user = await User.login(
+          browser, test.info(), 
+          'normal', '(mot de passe)'
+        ); 
+        await use(user);
+        await user.resetState();
+      }, { scope: 'worker' }
+    ],
 });
