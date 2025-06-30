e72fa3f3853906553250ac1eae78f607b53ffcb3
Labasse <labasse@gmail.com>
2025-06-30 02:06:03 +0200

    Vérifier l'accessibilité (fin)

--- tests/accessibility.spec.ts
+++ tests/accessibility.spec.ts
@@ -1,19 +1,23 @@
 // npm install -D @axe-core/playwright
 import { test, expect } from '@playwright/test';
+import { AxeBuilder } from '@axe-core/playwright';
 
 
 test.describe('Accessibility testing', () => {
     
     test('Home page', async ({ page }) => {
         await page.goto('https://playwright.dev/');
+        const res = await new AxeBuilder({ page }).withTags('wcag2aa').analyze();
 
-        // TODO        
+        expect(res.violations).toEqual([]);
     });
 
     test('Documentation page', async ({ page }) => {
         await page.goto('https://playwright.dev/docs/intro');
         
-        // TODO
+        const res = await new AxeBuilder({ page }).withTags('wcag2aa').analyze();
+        
+        expect(res.violations).toEqual([]);
     });
 });
 
